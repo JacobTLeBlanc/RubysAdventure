a95913276815b1c2d40dc672d5fd6e0a55d95c03
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-06-30 00:09:12 -0300

    Added health collectibles + health bar

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Get Ruby Script as a variable
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.currentHealth < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }    
+        }
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5; // Max health of character
+    public int health { get { return currentHealth; }}
+    int currentHealth; // Var to keep track of current hp
+
+    public float speed = 3.0f; // Speed of character
+
     Rigidbody2D rigidbody2d; // Rigidbody2d variable
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>(); // Get rb2d component of game object
+
+        currentHealth = maxHealth; // Set health to full at start
     }
 
     // Update is called once per frame
@@ -20,9 +28,15 @@ public class RubyController : MonoBehaviour
 
         Vector2 position = rigidbody2d.position; // Create var and set to our pos
 
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime; // Update new pos if needed
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime; // Update new pos if needed
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position); // Set our pos to new pos
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0 , maxHealth); // Make sure health is between 0 and 5
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
