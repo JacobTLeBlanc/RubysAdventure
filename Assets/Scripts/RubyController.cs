f5d25b26d0c72fb9e3093a2b0eae7a72a13e6582
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-05 21:36:15 -0300

    Added NPC, and NPC dialog

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -68,6 +68,20 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
 
     public void ChangeHealth(int amount)
